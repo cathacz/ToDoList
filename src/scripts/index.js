95bc40f7193a7ab741587a3156c81123570659ca
catha <catharinaczipf@hotmail.com>
2021-04-26 14:54:04 +0200

    get your $hit done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,68 @@
+const create = () => {
+  // creation
+  const newElement = document.createElement("div");
+  const newText = document.createTextNode("Catha fetzt");
+  // injection (text into div > div into document)
+  // div                // text
+  newElement.appendChild(newText);
+  document.body.appendChild(newElement);
+};
+
 const add2List = () => {
   let userData = document.querySelector("#userData").value;
+  //   console.log(userData);
   if (userData != "") {
     let newLi = document.createElement("li");
     let text = document.createTextNode(userData);
     newLi.appendChild(text);
+    // newLi.style.color = colorGen();
     document.querySelector(".result").appendChild(newLi);
     document.querySelector("#userData").value = "";
+    const listItems = document.querySelectorAll("li");
+    listItems.forEach((item) => {
+      item.style.color = colorGen();
+    });
+    const newContainer = document.createElement("div");
+    const doneButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    newContainer.appendChild(doneButton);
+    newContainer.appendChild(deleteButton);
+    newLi.appendChild(newContainer);
+
+    // CHECKBOX
+    // const checkBox = document.createElement("input");
+    // checkBox.type = "checkbox";
+    // newContainer.appendChild(checkBox);
+
+    doneButton.innerHTML = "done";
+    deleteButton.innerHTML = "skip";
+
+    const check = () => {
+      newLi.classList.innerHTML.toggle("completed");
+    };
+    const del = () => {
+      newLi.remove();
+    };
+    doneButton.addEventListener("click", check);
+    deleteButton.addEventListener("click", del);
   } else {
     document.querySelector("#userData").placeholder = "Type something first!";
   }
 };
 
-// Get the input field
-const input = document.getElementById("userData");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function (event) {
-  if (event.key == 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("add").click();
+const colorGen = () => {
+  let result = "#";
+  let colorCode = "1234567890ABCDEF";
+  for (let i = 0; i < 6; i++) {
+    result += colorCode[Math.floor(Math.random() * 16)];
   }
-});
+  return result;
+};
+
+const keyCheck = (event) => {
+  if (event.key == "Enter") add2List();
+};
+
+const userInput = document.querySelector("#userData");
+userInput.addEventListener("keypress", keyCheck);
